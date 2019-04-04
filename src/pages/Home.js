5a8a062d941ea51019a8abe938f442f055ce231c
Gal Vishnevsky <gal.vishnevsky@ironsrc.com>
2019-04-04 21:39:06 +0300

    firebase

--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Home() {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
-}
--- src/pages/Topic.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Topic({match}) {
-    return (
-        <div>
-            <h3>{match.params.topicId}</h3>
-        </div>
-    );
-}
