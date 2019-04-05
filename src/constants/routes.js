b1e4cd86c485c59d815c4f5d70327279709eb5b9
eladbarack <elad@spotinst.com>
2019-04-05 04:59:36 +0300

    Second Question

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -7,3 +7,4 @@ export const QUESTION        = '/question';
 export const PASSWORD_FORGET = '/pw-forget';
 export const ADMIN           = '/admin';
 export const ADMIN_DETAILS   = '/admin/:id';
+export const MATCH           = '/match';
